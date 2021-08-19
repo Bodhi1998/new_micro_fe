a4ca0cc7bd61bad2069149c24df51f49a3c6e1ab
iamrahulrnair <rahulrnair007@outlook.com>
2021-08-19 16:09:18 +0530

    check yaml edits

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const moduleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJSON = require("../package.json");
-
+console.log("checks");
 const commonConfig = require("./webpack.common");
 const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
