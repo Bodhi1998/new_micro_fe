62632c8ab9ddc34dde547e45c9e2b2be1994c201
iamrahulrnair <rahulrnair007@outlook.com>
2021-08-26 15:35:07 +0530

    add merketing and changes to container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
     new moduleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry`,
       },
       shared: packageJSON.dependencies,
     }),
